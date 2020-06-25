ff293a4b9e82e2370bbfaceebb3268fa9565d9fa
Brandon <brandonnguyen3301@gmail.com>
2020-06-25 16:59:19 -0700

    fixed some crashes. fixed universal shop

--- SpiritMS Src/scripts/npc/9000019.js
+++ SpiritMS Src/scripts/npc/9000019.js
@@ -3,6 +3,7 @@ var status = -1;
 var mapleLeaf = 4001126;
 var isWeapon = false;
 var itemSelected;
+var mesoBag = 5200002;
 
 var options = ["I want to exchange maple leaves for Pearl Maple Gear.", "I want to exchange maple leaves for Onyx Maple Gear."];
 
@@ -33,8 +34,32 @@ var onyxEqp = [ // Maple suit and Ring have wrong IDs TODO: Find them
 	1012376 // Onyx Maple Gum
 ];
 
-var pearlWep = [];
-var pearlEqp = [];
+var pearlWep = [
+	1532102, // Pearl Maple Cannon
+	1522098, // Pearl Maple Twin Angels
+	1492183, // Pearl Maple Shooter
+	1482172, // Pearl Maple Grip
+	1472218, // Pearl Maple Steer
+	1462197, // Pearl Maple Crossbow
+	1452209, // Pearl Maple Longbow
+	1442227, // Pearl Maple Polearm
+	1432171, // Pearl Maple Spear
+	1422144, // Pearl Maple Maul
+	1412139, // Pearl Maple Two-handed Axe
+	1402202, // Pearl Maple Two-handed Sword
+	1382213, // Pearl Maple Staff
+	1372181, // Pearl Maple Wand
+	1362094, // Pearl Maple Cane
+	1332229, // Pearl Maple Cutter
+	1322207 // Pearl Maple Mace
+];
+var pearlEqp = [
+	1102563, // Pearl Maple Cape
+	1003864, // Pearl Maple Hat
+	1122253, // Pearl Maple Pendant
+	//1132229, // Pearl Maple Buckle
+	1012377 // Pearl Maple Gum
+];
 
 
 var optionSelected;
@@ -66,14 +91,91 @@ function action(mode, type, selection) {
 		
 		cm.sendSimple(msg);
 	}
+	// Pearl Gear
 	else if(status == 1 && selection == 0)
 	{
 		optionSelected = selection; // Pearl Eqp
+		var pearlDiag = "Which type of pearl maple gear would you like to trade for\r\n";
+		pearlDiag += "#L2##bTrade for weapons.\r\n";
+		pearlDiag += "#L3#Trade for equipments.#l";
+		cm.sendSimple(pearlDiag);
 	}
+	else if(status == 2 && optionSelected == 0 && selection == 2) // Weapons
+	{
+		isWeapon = true;
+		var wepDiag = "Which weapon would you like to trade for?\r\n"
+		for(var i = 0; i < pearlWep.length; i++){
+			wepDiag += "#L" + i + "##b#i" + pearlWep[i] + "##z" + pearlWep[i] + "#\r\n";
+		}
+		cm.sendSimple(wepDiag);
+	}
+	else if(status == 2 && optionSelected == 0 && selection == 3) // Equips
+	{
+		var eqpDiag = "Which equipment would you like to trade for?\r\n"
+		for(var i = 0; i < pearlEqp.length; i++){
+			eqpDiag += "#L" + i + "##b#i" + pearlEqp[i] + "##z" + pearlEqp[i] + "#\r\n";
+		}
+		cm.sendSimple(eqpDiag);
+	}
+	else if(status == 3 && optionSelected == 0 && isWeapon)
+	{
+		itemSelected = selection;
+		var confirmDiag = "This is the item you want to buy?#i" + pearlWep[itemSelected] + "#\r\n\r\n"
+		confirmDiag += "Here is the required materials needed to buy it.\r\n";
+		confirmDiag += "#i" + mapleLeaf + "# 2500\r\n";
+		confirmDiag += "#i" + mesoBag + "# 3,000,000";
+		
+		cm.sendYesNo(confirmDiag);
+	}
+	else if(status == 3 && optionSelected == 0 && !isWeapon)
+	{
+		itemSelected = selection;
+		var confirmDiag = "This is the item you want to buy?#i" + pearlEqp[itemSelected] + "#\r\n\r\n"
+		confirmDiag += "Here is the required materials needed to buy it.\r\n";
+		confirmDiag += "#i" + mapleLeaf + "# 2500\r\n";
+		confirmDIag += "#i" + mesoBag + "# 3,000,000";
+		
+		cm.sendYesNo(confirmDiag);
+	}
+	else if(status == 4 && optionSelected == 0 && isWeapon)
+	{
+		if(cm.getQuantityOfItem(mapleLeaf) < 2500){
+			cm.sendOk("You do not have enough maple leaves, come back again later.");
+			return cm.dispose();
+		}
+		if(cm.getMeso() < 3000000){
+			cm.sendOk("You do not have enough mesos, come back again later.");
+			return cm.dispose();
+		}
+		
+		cm.gainItem(mapleLeaf, -2500);
+		cm.getChar().gainMeso(-3000000)
+		cm.gainItem(pearlWep[itemSelected], 1);
+		cm.sendOk("Successfully purchased a #i" + pearlWep[itemSelected] + "#");
+		return cm.dispose();
+	}
+	else if(status == 4 && optionSelected == 0 && !isWeapon)
+	{
+		if(cm.getQuantityOfItem(mapleLeaf) < 2500){
+			cm.sendOk("You do not have enough maple leaves, come back again later.");
+			return cm.dispose();
+		}
+		if(cm.getMeso() < 3000000){
+			cm.sendOk("You do not have enough mesos, come back again later.");
+			return cm.dispose();
+		}
+		cm.gainItem(mapleLeaf, -2500);
+		cm.getChar().gainMeso(-3000000)
+		cm.gainItem(pearlEqp[itemSelected], 1);
+		cm.sendOk("Successfully purchased a #i" + pearlEqp[itemSelected] + "#");
+		return cm.dispose();
+	}
+	
+	// Onyx Gear
 	else if(status == 1 && selection == 1)
 	{
 		optionSelected = selection; // Onyx Eqp
-		var onyxDiag = "Which onyx type of onyx maple gear would you like to trade for?\r\n"
+		var onyxDiag = "Which type of onyx maple gear would you like to trade for?\r\n";
 		onyxDiag += "#L2##bTrade for weapons.\r\n";
 		onyxDiag += "#L3#Trade for equipments.#l";
 		cm.sendSimple(onyxDiag);
@@ -100,7 +202,9 @@ function action(mode, type, selection) {
 		itemSelected = selection;
 		var confirmDiag = "This is the item you want to buy?#i" + onyxWep[itemSelected] + "#\r\n\r\n"
 		confirmDiag += "Here is the required materials needed to buy it.\r\n";
-		confirmDiag += "#i" + mapleLeaf + "# 5000";
+		confirmDiag += "#i" + mapleLeaf + "# 5000\r\n";
+		confirmDiag += "#i" + mesoBag + "# 5,000,000\r\n";
+		confirmDiag += "#i" + pearlWep[itemSelected] + "# 1";
 		
 		cm.sendYesNo(confirmDiag);
 	}
@@ -109,7 +213,9 @@ function action(mode, type, selection) {
 		itemSelected = selection;
 		var confirmDiag = "This is the item you want to buy?#i" + onyxEqp[itemSelected] + "#\r\n\r\n"
 		confirmDiag += "Here is the required materials needed to buy it.\r\n";
-		confirmDiag += "#i" + mapleLeaf + "# 5000";
+		confirmDiag += "#i" + mapleLeaf + "# 5000\r\n";
+		confirmDiag += "#i" + mesoBag + "# 5,000,000\r\n";
+		confirmDiag += "#i" + pearlEqp[itemSelected] + "# 1";
 		
 		cm.sendYesNo(confirmDiag);
 	}
@@ -120,13 +226,19 @@ function action(mode, type, selection) {
 			return cm.dispose();
 		}
 		
-		//if(cm.getQuantityOfItem(pearlWep[itemSelected]) <= 0){
-		//	cm.sendOk("You do not have the neccessary materials, come back again later.");
-		//	return cm.dispose();
-		//}
+		if(cm.getQuantityOfItem(pearlWep[itemSelected]) <= 0){
+			cm.sendOk("You do not have the neccessary materials, come back again later.");
+			return cm.dispose();
+		}
+		
+		if(cm.getMeso() < 5000000){
+			cm.sendOk("You do not enough mesos, come back again later.");
+			return cm.dispose();
+		}
 		
 		cm.gainItem(mapleLeaf, -5000);
-		//cm.gainItem(pearlWep[itemSelected], -1);
+		cm.getChar().gainMeso(-5000000)
+		cm.gainItem(pearlWep[itemSelected], -1);
 		cm.gainItem(onyxWep[itemSelected], 1);
 		cm.sendOk("Successfully purchased a #i" + onyxWep[itemSelected] + "#");
 		return cm.dispose();
@@ -137,14 +249,18 @@ function action(mode, type, selection) {
 			cm.sendOk("You do not have enough maple leaves, come back again later.");
 			return cm.dispose();
 		}
-		
-		//if(cm.getQuantityOfItem(pearlEqp[itemSelected]) <= 0){
-		//	cm.sendOk("You do not have the neccessary materials, come back again later.");
-		//	return cm.dispose();
-		//}
+		if(cm.getQuantityOfItem(pearlEqp[itemSelected]) <= 0){
+			cm.sendOk("You do not have the neccessary materials, come back again later.");
+			return cm.dispose();
+		}
+		if(cm.getMeso() < 5000000){
+			cm.sendOk("You do not enough mesos, come back again later.");
+			return cm.dispose();
+		}
 		
 		cm.gainItem(mapleLeaf, -5000);
-		//cm.gainItem(pearlEqp[itemSelected], -1);
+		cm.gainItem(pearlEqp[itemSelected], -1);
+		cm.getChar().gainMeso(-5000000)
 		cm.gainItem(onyxEqp[itemSelected], 1);
 		cm.sendOk("Successfully purchased a #i" + onyxEqp[itemSelected] + "#");
 		return cm.dispose();
--- SpiritMS Src/scripts/npc/9010036.js
+++ SpiritMS Src/scripts/npc/9010036.js
@@ -3,7 +3,7 @@ var status = -1;
 //var itemsa = Array(2550, 20000, 30000, 5000, 4100, 2550, 4100, 5000, 50000, 2000, 5000, 6000, 7500, 7500, 8000, 8000, 9000, 9000, 9000, 9000, 9000, 9000, 9000, 9000, 6000, 9000, 4500, 80000, 150000, 100000, 35000, 3400, 11800, 19800, 20000, 20000, 40000, 1000, 7000);
 //var itemse = Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 14, -1, 30, -1, -1, 1, 7, 14, -1, -1, -1, -1);
 var items = [
-/*Cubes*/ [[5062009, 11000, -1], [5062500, 25000, -1]],
+/*Cubes*/ [[5062009, 25000, -1], [5062500, 50000, -1]],
 /*Scrolls*/ [[2049100, 5100, -1], [2040804, 9000, -1], [2040301, 9000, -1], [2046223, 7000, -1], [2046224, 7000, -1], [2046225, 7000, -1], [2046226, 7000, -1], [2046314, 7000, -1], [2046315, 7000, -1], [2046316, 7000, -1], [2046317, 7000, -1], [2040817, 9000, -1]],
 /*Scrolling Tools*/ [/*[2340000, 50000, -1], */[5610000, 10000, -1], [5610001, 12000, -1], [5640000, 80000, -1], [2531000, 100000, -1], [2530000, 35000, -1], [5534000, 20000, -1]],
 /*Game Enhancing*/ [[5050000, 2050, -1], [5510000, 1000, -1], [5521000, 10000, -1], [5062200, 5000, -1], [5062201, 10000, -1], [5133000, 1000, -1], [5520001, 8000, -1]],
@@ -26,7 +26,7 @@ function action(mode, type, selection) {
     status++;
     if (status == 0) {
         //cm.sendSimple("Hello there, I am trading Cash for items. It seems that you have #r#e" + cm.getPlayer().getCSPoints(1) + "#n#k #rCash#k. You could sure waste some...\r\n\r\n#b#L0#Give me #rCash#b and I'll give you an item.#l#k");
-        cm.sendSimple("Hi, I like #rCash#k. Maybe you could trade me some of your #r" + cm.getPlayer().getCSPoints(1) + " Cash#k? I have lots of great items for you...#b\r\n\r\n#L0#Cubes#l\r\n#L1#Scrolls#l\r\n#L2#Scrolling Tools#l\r\n#L3#Game-enhancing#l\r\n#L4#Hired Merchant#l\r\n#L5#ETC#l\r\n#L6#Mastery Books#l");
+        cm.sendSimple("Hi, I like #rCash#k. Maybe you could trade me some of your #r" + cm.getPlayer().getMaplePoints() + " Cash#k? I have lots of great items for you...#b\r\n\r\n#L0#Cubes#l\r\n#L1#Scrolls#l\r\n#L2#Scrolling Tools#l\r\n#L3#Game-enhancing#l\r\n#L4#Hired Merchant#l\r\n#L5#ETC#l\r\n#L6#Mastery Books#l");
     } else if (status == 1) {
         select = selection;
         var selStr = "Which item would you like?\r\n#b";
@@ -37,12 +37,12 @@ function action(mode, type, selection) {
     } else if (status == 2) {
         select2 = selection;
         if (items[select][selection][0] / 1000000 == 1) {
-            if (cm.getPlayer().getCSPoints(1) < items[select2][i][1] / 2) {
+            if (cm.getPlayer().getMaplePoints() < items[select2][i][1] / 2) {
                 cm.sendOk("It seems that you don't have enough #rCash#k.");
             } else if (!cm.canHold(items[select][select2][0], 1)) {
                 cm.sendOk("You don't have the inventory space to hold it. I must be legit and make this a fair trade... so hurry up and free your inventory.");
             } else {
-                cm.getPlayer().modifyCSPoints(1, -(items[select][select2][i][1] / 2), true);
+                cm.getPlayer().gainMaplePoints(-(items[select][select2][i][1] / 2));
                 if (items[select][select2][2] > 0) {
                     cm.gainItemPeriod(items[select][select2][0], 1, items[select][select2][2]);
                 } else {
@@ -52,19 +52,19 @@ function action(mode, type, selection) {
             }
             cm.dispose();
         } else {
-            cm.sendGetNumber("How many would you like? (1#v" + items[select][selection][0] + "##t" + items[select][selection][0] + "# = " + items[select][selection][1] / 2 + " Cash) (Current Cash: " + cm.getPlayer().getCSPoints(1) + ")", 1, 1, cm.getPlayer().getCSPoints(1) / (items[select][selection][1] / 2));
+            cm.sendGetNumber("How many would you like? (1#v" + items[select][selection][0] + "##t" + items[select][selection][0] + "# = " + items[select][selection][1] / 2 + " Cash) (Current Cash: " + cm.getPlayer().getMaplePoints() + ")", 1, 1, cm.getPlayer().getMaplePoints() / (items[select][selection][1] / 2));
         }
     } else if (status == 3) {
         if ((items[select][select2][0] == 2340000 || items[select][select2][0] == 5610000 || items[select][select2][0] == 5610001 || items[select][select2][0] == 5062001 || items[select][select2][0] == 5640000) && cm.getPlayer().getLevel() < 70) {
             cm.sendOk("Sorry but you must be level 70 or above to get this item.");
         } else if (items[select][select2][0] == 2022179 && cm.getPlayer().getLevel() < 50) {
             cm.sendOk("Sorry but you must be level 50 or above to get this item.");
-        } else if (cm.getPlayer().getCSPoints(1) < items[select][select2][1] / 2) {
+        } else if (cm.getPlayer().getMaplePoints() < items[select][select2][1] / 2) {
             cm.sendOk("It seems that you don't have enough #rCash#k.");
         } else if (!cm.canHold(items[select][select2][0], selection)) {
             cm.sendOk("You don't have the inventory space to hold it. I must be legit and make this a fair trade... so hurry up and free your inventory.");
         } else {
-            cm.getPlayer().modifyCSPoints(1, -(items[select][select2][1] / 2 * selection), true);
+            cm.getPlayer().gainMaplePoints(-(items[select][select2][1] / 2 * selection));
             if (items[select][select2][2] > 0) {
                 cm.gainItemPeriod(items[select][select2][0], selection, items[select][select2][2]);
             } else {

8cf2873a60a5e25cd8173ffaaf26fbecea74301d
Descended <41481611+Descended@users.noreply.github.com>
2020-07-01 23:21:30 +0200

    Added !whisper command for the discord bot

--- SpiritMS Src/src/main/java/bot/SpiritBot.java
+++ SpiritMS Src/src/main/java/bot/SpiritBot.java
@@ -9,8 +9,8 @@ import javax.security.auth.login.LoginException;
 public class SpiritBot {
 
     public static void start() throws Exception{
-        JDA jda = (JDA) new JDABuilder("NzIyMTgwNTY3NTUwMTMyMjI1.XugTMw.krrXIM697aMVMHX11WCT3BuCqfk").build();
-        jda.addEventListener(new DisconnectHandler(), new InfoHandler(), new SaveAllHandler(), new OnlineHandler(), new ShutDownHandler(), new ShutDownTimeHandler());
+        JDA jda = (JDA) new JDABuilder("TokenIDHERE").build();
+        jda.addEventListener(new DisconnectHandler(), new InfoHandler(), new SaveAllHandler(), new OnlineHandler(), new ShutDownHandler(), new ShutDownTimeHandler(), new WhisperHandler());
     }
 
 }

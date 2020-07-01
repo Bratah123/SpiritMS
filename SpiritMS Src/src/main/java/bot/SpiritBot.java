4095dede6c28039e363eda6c42dd6def629f8581
Brandon <brandonnguyen3301@gmail.com>
2020-07-01 15:06:11 -0700

    fixed errors

--- SpiritMS Src/src/main/java/bot/SpiritBot.java
+++ SpiritMS Src/src/main/java/bot/SpiritBot.java
@@ -1,7 +1,6 @@
 package bot;
 
 import bot.event.*;
-import com.mrpowergamerbr.temmiewebhook.TemmieWebhook;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
